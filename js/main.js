38d35aeabcdaae0afbbbfde9dcb0b6457631bc71
Saubhagya Rai <raisaubhagya@gmail.com>
2021-06-25 15:51:13 +0545

    Design and Templating

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//Main Slider
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: false,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 5000,
+  });
+});
+
+// Alumni Slider
+
+$(document).ready(function () {
+  $(".alumni-slider").slick({
+    dots: false,
+    autoplay: true,
+    arrows: true,
+    autoplaySpeed: 5000,
+  });
+});
